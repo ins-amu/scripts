c96877efb7a3f43af54a1db71622248b4c389ef4
tim <tim@tym>
2014-03-31 17:33:14 +0200

    small fix

--- main.sh
+++ main.sh
@@ -11,99 +11,99 @@ export BV=/home/tim/Work/Soft/brainvisa-4.3.0/
 alias matlab=/home/tim/Matlab/bin/matlab
 # error handling
 set -e
-# ########## build cortical surface and region mapping
+########## build cortical surface and region mapping
 # cd $PRD/scripts
 # mrconvert $PRD/data/T1/ T1.nii
-# ###################### freesurfer
-# # recon-all -i $PRD/data/T1/01_0027_t1-mprage-sag-iso1-0mm/original-primary-m-nd-norm_e01_0001.dcm -s $SUBJ_ID -all
-# 
-# 
-# ###################################### left hemisphere
-# # export pial into text file
-# mkdir -p ../surface
-# mris_convert $FS/$SUBJ_ID/surf/lh.pial $PRD/surface/lh.pial.asc
-# 
-# 
-# # change pial : erase first two lines, get number vertices and triangles
-# tail -n +3 $PRD/surface/lh.pial.asc > $PRD/surface/lh_pial.txt
-# sed -n '2p' $PRD/surface/lh.pial.asc | awk '{print $1}' > $PRD/surface/lh_number_vertices_high.txt
-# sed -n '2p' $PRD/surface/lh.pial.asc | awk '{print $2}' > $PRD/surface/lh_number_triangles_high.txt
-# 
-# # triangles and vertices high
-# python left_extract_high.py
-# 
-# # -> to mesh
-# $BV/bin/python left_transform_mesh_high.py
-# 
-# #  decimation
-# $BV/bin/AimsMeshDecimation $PRD/surface/lh_mesh_high.mesh $PRD/surface/lh_mesh_low.mesh
-# 
-# # export to list vertices triangles
-# $BV/bin/python left_export_to_vertices.py
-# 
-# # create left the region mapping
-# matlab -r "run left_region_mapping.m; quit;" -nodesktop -nodisplay
-# 
-# # check
-# if [ -n "$DISPLAY" ]
-# then
-# python check_left_region_mapping.py
-# fi
-# 
-# # correct
-# python correct_left_region_mapping.py
-# 
-# ###################################### right hemisphere
-# # export pial into text file
-# mris_convert $FS/$SUBJ_ID/surf/rh.pial $PRD/surface/rh.pial.asc
-# 
-# # change pial : erase first two lines, get number vertices and triangles
-# tail -n +3 $PRD/surface/rh.pial.asc > $PRD/surface/rh_pial.txt
-# sed -n '2p' $PRD/surface/rh.pial.asc | awk '{print $1}' > $PRD/surface/rh_number_vertices_high.txt
-# sed -n '2p' $PRD/surface/rh.pial.asc | awk '{print $2}' > $PRD/surface/rh_number_triangles_high.txt
-# 
-# # triangles and vertices high
-# python right_extract_high.py
-# 
-# # -> to mesh
-# $BV/bin/python right_transform_mesh_high.py
-# 
-# #  decimation
-# $BV/bin/AimsMeshDecimation $PRD/surface/rh_mesh_high.mesh $PRD/surface/rh_mesh_low.mesh
-# 
-# # export to list vertices triangles
-# $BV/bin/python right_export_to_vertices.py
-# 
-# # create left the region mapping
-# matlab -r "run right_region_mapping.m; quit;" -nodesktop -nodisplay
-# 
-# # check
-# if [ -n "$DISPLAY" ]; then python check_right_region_mapping.py; fi
-# 
-# # correct
-# python correct_right_region_mapping.py
-# 
-# ###################################### both hemisphere
-# # prepare final directory
-# mkdir -p $PRD/$SUBJ_ID
-# mkdir -p $PRD/$SUBJ_ID/surfaces
-# 
-# # reunify both region_mapping, vertices and triangles
-# python reunify_both_regions.py
-# 
-# # zip to put in final format
-# cd $PRD/$SUBJ_ID/surfaces/scripts
-# zip $PRD/$SUBJ_ID/surface.zip vertices.txt triangles.txt
-# cd $PRD/scripts
-# 
-# ########################### subcortical surfaces
-# # extract subcortical surfaces 
-# ./aseg2srf -s $SUBJ_ID
-# mkdir -p $PRD/surface/subcortical
-# cp $FS/$SUBJ_ID/ascii/* $PRD/surface/subcortical
-# python list_subcortical.py
-# 
-# 
+###################### freesurfer
+# recon-all -i $PRD/data/T1/01_0027_t1-mprage-sag-iso1-0mm/original-primary-m-nd-norm_e01_0001.dcm -s $SUBJ_ID -all
+
+
+###################################### left hemisphere
+# export pial into text file
+mkdir -p ../surface
+mris_convert $FS/$SUBJ_ID/surf/lh.pial $PRD/surface/lh.pial.asc
+
+
+# change pial : erase first two lines, get number vertices and triangles
+tail -n +3 $PRD/surface/lh.pial.asc > $PRD/surface/lh_pial.txt
+sed -n '2p' $PRD/surface/lh.pial.asc | awk '{print $1}' > $PRD/surface/lh_number_vertices_high.txt
+sed -n '2p' $PRD/surface/lh.pial.asc | awk '{print $2}' > $PRD/surface/lh_number_triangles_high.txt
+
+# triangles and vertices high
+python left_extract_high.py
+
+# -> to mesh
+$BV/bin/python left_transform_mesh_high.py
+
+#  decimation
+$BV/bin/AimsMeshDecimation $PRD/surface/lh_mesh_high.mesh $PRD/surface/lh_mesh_low.mesh
+
+# export to list vertices triangles
+$BV/bin/python left_export_to_vertices.py
+
+# create left the region mapping
+matlab -r "run left_region_mapping.m; quit;" -nodesktop -nodisplay
+
+# check
+if [ -n "$DISPLAY" ]
+then
+python check_left_region_mapping.py
+fi
+
+# correct
+python correct_left_region_mapping.py
+
+###################################### right hemisphere
+# export pial into text file
+mris_convert $FS/$SUBJ_ID/surf/rh.pial $PRD/surface/rh.pial.asc
+
+# change pial : erase first two lines, get number vertices and triangles
+tail -n +3 $PRD/surface/rh.pial.asc > $PRD/surface/rh_pial.txt
+sed -n '2p' $PRD/surface/rh.pial.asc | awk '{print $1}' > $PRD/surface/rh_number_vertices_high.txt
+sed -n '2p' $PRD/surface/rh.pial.asc | awk '{print $2}' > $PRD/surface/rh_number_triangles_high.txt
+
+# triangles and vertices high
+python right_extract_high.py
+
+# -> to mesh
+$BV/bin/python right_transform_mesh_high.py
+
+#  decimation
+$BV/bin/AimsMeshDecimation $PRD/surface/rh_mesh_high.mesh $PRD/surface/rh_mesh_low.mesh
+
+# export to list vertices triangles
+$BV/bin/python right_export_to_vertices.py
+
+# create left the region mapping
+matlab -r "run right_region_mapping.m; quit;" -nodesktop -nodisplay
+
+# check
+if [ -n "$DISPLAY" ]; then python check_right_region_mapping.py; fi
+
+# correct
+python correct_right_region_mapping.py
+
+###################################### both hemisphere
+# prepare final directory
+mkdir -p $PRD/$SUBJ_ID
+mkdir -p $PRD/$SUBJ_ID/surfaces
+
+# reunify both region_mapping, vertices and triangles
+python reunify_both_regions.py
+
+# zip to put in final format
+cd $PRD/$SUBJ_ID/surfaces/scripts
+zip $PRD/$SUBJ_ID/surface.zip vertices.txt triangles.txt
+cd $PRD/scripts
+
+########################### subcortical surfaces
+# extract subcortical surfaces 
+./aseg2srf -s $SUBJ_ID
+mkdir -p $PRD/surface/subcortical
+cp $FS/$SUBJ_ID/ascii/* $PRD/surface/subcortical
+python list_subcortical.py
+
+
 ########################## build connectivity
 # mrtrix
 mkdir -p $PRD/connectivity
