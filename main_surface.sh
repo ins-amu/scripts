302610a397d9caef75466b0629a9ed34ec98e51a
tim <tim@tym>
2014-04-27 22:44:58 +0200

    change main

--- main_surface.sh
+++ main_surface.sh
@@ -1,39 +1,4 @@
-############### #
-#the root directory for files 
-export PRD=/home/tim/Work/Processed_data/tim_pipeline/TREC/
-# freesurfer 
-export FS=$SUBJECTS_DIR
-# subject name
-export SUBJ_ID=TREC
-# brainvisa directory
-export BV=/home/tim/Work/Soft/brainvisa-4.3.0/
-# matlab path
-alias matlab='/usr/local/MATLAB/R2013a/bin/matlab'
-# error handling
-#set -e
-# checking
-export CHECK=no
-
-########## Important parameters
-# This parameter modify the mask for diffusion processing
-# check for different values with mrview
-export percent_value_mask=10
-
-# This parameter is the maximum harmonic order for spherical deconvolution
-# It depends of the number of directions used during acquisition
-# Please refer to the following table 
-# Maximum harmonic order (lmax)	Number of parameters required
-#             2				     6
-#             4				     15
-#             6				     28
-#             8				     45
-#             10			     66
-#             12			     91
-#             n				½ (n+1)(n+2)
-export lmax=6 
 ######### import config
-#!/bin/bash
- 
 while getopts ":config:" opt; do
 case $opt in
  config)
