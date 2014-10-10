10c0600474b4ea2d5d9763799cdab0872f956567
tim <tim@tym>
2014-10-10 16:14:39 +0200

    change for HCP data

--- main_surface.sh
+++ main_surface.sh
@@ -182,8 +182,7 @@ cp $FS/$SUBJ_ID/ascii/* $PRD/surface/subcortical
 python list_subcortical.py
 fi
 
-########################## build connectivity
-# Now using mrtrix 3 
+########################## build connectivity using mrtrix 3
 mkdir -p $PRD/connectivity
 mkdir -p $PRD/$SUBJ_ID/connectivity
 # mrconvert
@@ -196,6 +195,133 @@ else
 mrconvert $PRD/data/DWI/ $PRD/connectivity/dwi.mif
 fi
 fi
+
+############################
+#        HCP DATA          #
+############################
+
+if [ "$HCP" = "yes"]
+
+# copy the data already available
+# see handle_HCP_data.py
+
+dwi2response -grad $PRD/data/DWI/encoding.b $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -mask $PRD/connectivity/mask.mif -lmax 6
+
+
+# response function estimation
+if [ ! -f $PRD/connectivity/response.txt ]
+then
+if [ -f $PRD/data/DWI/*.nii ]
+then
+ls $PRD/data/DWI/ | grep '.b$' | xargs -I {} dwi2response $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -grad $PRD/data/DWI/{}
+else
+dwi2response $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt
+fi
+fi
+
+
+# fibre orientation distribution estimation
+if [ ! -f $PRD/connectivity/CSD6.mif ]
+then
+ls $PRD/data/DWI/ | grep '.b$' | xargs -I {} dwi2fod $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt $PRD/connectivity/CSD$lmax.mif -lmax $lmax -mask $PRD/connectivity/mask.mif -grad $PRD/data/DWI/{} 
+fi
+
+# FLIRT registration
+#Diff to T1
+if [ ! -f $PRD/connectivity/T1.nii ]
+then
+echo "generating good orientation for T1"
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FS/$SUBJ_ID/mri/T1.mgz $PRD/connectivity/T1.nii
+fi
+if [ ! -f $PRD/connectivity/aparc+aseg.nii ]
+then
+echo " getting aparc+aseg"
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FS/$SUBJ_ID/mri/aparc+aseg.mgz $PRD/connectivity/aparc+aseg.nii
+fi
+
+
+if [ ! -f $PRD/connectivity/aparc+aseg_reorient.nii ]
+then
+echo "reorienting the region parcellation"
+fslreorient2std $PRD/connectivity/aparc+aseg.nii $PRD/connectivity/aparc+aseg_reorient.nii
+# check parcellation to T1
+if [ -n "$DISPLAY" ] && [ "$CHECK" = "yes" ]
+then
+echo "check parcellation"
+echo " if it's correct, just close the window. Otherwise... well, it should be correct anyway"
+fslview $PRD/connectivity/T1.nii $PRD/connectivity/aparc+aseg_reorient -l "Cool"
+fi
+fi
+
+if [ ! -f $PRD/connectivity/aparcaseg_2_diff.nii.gz ]
+then
+echo " register aparc+aseg to diff"
+flirt -in $PRD/connectivity/T1w_acpc_dc_restore_1.25.nii.gz -ref $PRD/connectivity/T1.nii -omat $PRD/connectivity/diffusion_2_struct.mat -out $PRD/connectivity/lowb_2_struct.nii -dof 6 -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -cost mutualinfo
+convert_xfm -omat $PRD/connectivity/diffusion_2_struct_inverse.mat -inverse $PRD/connectivity/diffusion_2_struct.mat
+flirt -applyxfm -in $PRD/connectivity/aparc+aseg_reorient.nii -ref $PRD/connectivity/T1w_acpc_dc_restore_1.25.nii.gz -out $PRD/connectivity/aparcaseg_2_diff.nii -init $PRD/connectivity/diffusion_2_struct_inverse.mat -interp nearestneighbour
+# check parcellation to diff
+if [ -n "$DISPLAY" ]  && [ "$CHECK" = "yes" ]
+then
+echo "check parcellation registration to diffusion space"
+echo "if it's correct, just close the window. Otherwise you will have to
+do the registration by hand"
+fslview $PRD/connectivity/T1w_acpc_dc_restore_1.25.nii.gz $PRD/connectivity/aparcaseg_2_diff -l "Cool"
+fi
+fi
+
+# tractography
+if [ ! -f $PRD/connectivity/whole_brain_1.tck ]
+then
+echo "generating tracks"
+for I in $(seq 1 $number_tracks)
+do
+tckgen $PRD/connectivity/CSD6.mif $PRD/connectivity/whole_brain_$I.tck -unidirectional -seed_image $PRD/connectivity/aparcaseg_2_diff.nii.gz -grad $PRD/data/DWI/encoding.b -mask $PRD/connectivity/mask.mif -num 100000 
+done
+fi
+
+# Connectome functions in mrtrix
+labelconfig $PRD/connectivity/aparcaseg_2_diff.nii.gz fs_region.txt $PRD/connectivity/aparcaseg_2_diff.mif -lut_freesurfer $FREESURFER_HOME/FreeSurferColorLUT.txt
+
+tck2connectome $PRD/connectivity/whole_brain_$I.tck $PRD/connectivity/aparcaseg_2_diff.mif $PRD/connectivity/weights.csv -assignment_radial_search 2
+
+tck2connectome -metric meanlength -zero_diagonal $PRD/connectivity/whole_brain_1.tck $PRD/connectivity/aparcaseg_2_diff.mif $PRD/connectivity/tract_lengths.csv
+
+# compute sub parcellations connectivity if asked
+
+if [ -n "$K" ]
+then
+export curr_K=$(( 2**K ))
+mkdir -p $PRD/$SUBJ_ID/connectivity_"$curr_K"
+if [ -n "$matlab" ]  
+then
+if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii ]
+then
+$matlab -r "run subparcel.m; quit;" -nodesktop -nodisplay 
+fi
+if [ ! -f $PRD/$SUBJ_ID/connectivity_"$curr_K"/weights.txt ]
+then
+$matlab -r "run compute_connectivity.m; quit;" -nodesktop -nodisplay
+fi
+else
+if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii ]
+then
+sh subparcel/distrib/run_subparcel.sh $MCR  
+fi
+if [ ! -f $PRD/$SUBJ_ID/connectivity_"$curr_K"/weights.txt ]
+then
+sh compute_connectivity/distrib/run_compute_connectivity.sh $MCR
+fi
+fi
+pushd .
+cd $PRD/$SUBJ_ID/connectivity_"$curr_K"
+zip $PRD/$SUBJ_ID/connectivity_"$curr_K".zip weights.txt tract_lengths.txt centres.txt
+popd
+fi
+
+else
+############################
+#      General Case        #
+############################
 
 # brainmask 
 if [ ! -f $PRD/connectivity/mask_not_checked.mif ]
@@ -229,7 +355,7 @@ then
 cp $PRD/connectivity/mask_not_checked.mif $PRD/connectivity/mask.mif
 fi
 
-dwi2response -grad $PRD/data/DWI/encoding.b $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -mask $PRD/connectivity/mask.mif
+dwi2response -grad $PRD/data/DWI/encoding.b $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -mask $PRD/connectivity/mask.mif -shell 1000,2000,3000 -lmax 6
 
 
 # response function estimation
@@ -249,11 +375,13 @@ if [ ! -f $PRD/connectivity/CSD6.mif ]
 then
 if [ -f $PRD/data/DWI/*.nii ]
 then
-ls $PRD/data/DWI/ | grep '.b$' | xargs -I {} dwi2fod $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -lmax $lmax -mask $PRD/connectivity/mask.mif $PRD/connectivity/CSD6.mif -grad $PRD/data/DWI/{}
+ls $PRD/data/DWI/ | grep '.b$' | xargs -I {} dwi2fod $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -lmax $lmax -mask $PRD/connectivity/mask.mif -grad $PRD/data/DWI/{} $PRD/connectivity/CSD6.mif 
 else
-dwi2fod $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -lmax $lmax -mask $PRD/connectivity/mask.mif $PRD/connectivity/CSD6.mif
+dwi2fod -lmax $lmax -mask $PRD/connectivity/mask.mif $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt $PRD/connectivity/CSD6.mif
 fi
 fi
+
+dwi2fod $PRD/connectivity/dwi.mif $PRD/connectivity/response.txt -lmax 6 -mask $PRD/connectivity/mask.mif $PRD/connectivity/CSD6.mif -grad  $PRD/data/DWI/encoding.b
 
 
 # FLIRT registration
@@ -286,7 +414,9 @@ fi
 if [ ! -f $PRD/connectivity/aparcaseg_2_diff.nii.gz ]
 then
 echo " register aparc+aseg to diff"
-flirt -in $PRD/connectivity/lowb.nii -ref $PRD/connectivity/T1.nii -omat $PRD/connectivity/diffusion_2_struct.mat -out $PRD/connectivity/lowb_2_struct.nii -dof 6 -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -cost mutualinfo
+
+flirt -in $PRD/connectivity/T1w_acpc_dc_restore_1.25.nii.gz -ref $PRD/connectivity/T1.nii -omat $PRD/connectivity/diffusion_2_struct.mat -out $PRD/connectivity/lowb_2_struct.nii -dof 6 -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -cost mutualinfo
+
 convert_xfm -omat $PRD/connectivity/diffusion_2_struct_inverse.mat -inverse $PRD/connectivity/diffusion_2_struct.mat
 flirt -applyxfm -in $PRD/connectivity/aparc+aseg_reorient.nii -ref $PRD/connectivity/lowb.nii -out $PRD/connectivity/aparcaseg_2_diff.nii -init $PRD/connectivity/diffusion_2_struct_inverse.mat -interp nearestneighbour
 #flirt -in $PRD/connectivity/aparc+aseg.nii -ref $PRD/connectivity/lowb.nii -out $PRD/connectivity/aparcaseg_2_diff.nii -interp nearestneighbour 
@@ -306,13 +436,13 @@ then
 echo "generating tracks"
 for I in $(seq 1 $number_tracks)
 do
-tckgen SD_PROB $PRD/connectivity/CSD6.mif -unidirectional -seed $PRD/connectivity/mask.mif -mask $PRD/connectivity/aparcaseg_2_diff.nii.gz $PRD/connectivity/whole_brain_$I.tck -num 100000
+tckgen $PRD/connectivity/CSD6.mif $PRD/connectivity/whole_brain_$I.tck -unidirectional -seed_image $PRD/connectivity/mask.mif -grad $PRD/data/DWI/encoding.b -mask $PRD/connectivity/mask.mif -num 10000 -minlength 50
 done
 fi
 
 
 # test new connectome functions in mrtrix
-labelconfig $PRD/connectivity/aparcaseg_2_diff.nii.gz fs_region.txt $PRD/connectivity/aparcaseg_2_diff.mif $FREESURFER_HOME/FreeSurferColorLUT.txt
+labelconfig $PRD/connectivity/aparcaseg_2_diff.nii.gz fs_region.txt $PRD/connectivity/aparcaseg_2_diff.mif -lut_freesurfer $FREESURFER_HOME/FreeSurferColorLUT.txt
 
 tck2connectome $PRD/connectivity/whole_brain_1.tck $PRD/connectivity/aparcaseg_2_diff.mif $PRD/connectivity/weights.csv
 tck2connectome -metric meanlength -zero_diagonal $PRD/connectivity/whole_brain_1.tck $PRD/connectivity/aparcaseg_2_diff.mif $PRD/connectivity/tract_lengths.csv
@@ -362,6 +492,7 @@ sh compute_connectivity/distrib/run_compute_connectivity.sh $MCR
 fi
 fi
 
+
 ########
 # we do not compute hemisphere
 # subcortical is already done
@@ -371,7 +502,7 @@ cp cortical.txt $PRD/$SUBJ_ID/connectivity/cortical.txt
 if [ ! -f $PRD/$SUBJ_ID/connectivity/centres.txt ]
 then
 echo " generate useful files for TVB"
-python compute_other_files.py
+python compute_region_files.py
 fi
 
 # zip to put in final format
