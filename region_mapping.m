b38949bd5f27807b77a3c11ca434fdf88d0a6e93
tim <tim@tym>
2014-10-21 12:03:22 +0200

    add files

---
+++ region_mapping.m
@@ -0,0 +1,27 @@
+if (~isdeployed)
+    addpath([FS, 'matlab'])
+end
+
+PRD = getenv('PRD')
+corr_right = load([rl, '_hemi_ref_table.txt']);
+FS = getenv('FS')
+SUBJ_ID = getenv('SUBJ_ID')
+[v, L, ct] = read_annotation([FS,'/',SUBJ_ID, '/label/', rl, '.aparc.annot']);
+cd(PRD)
+a = load(['surface/',rl ,'_vertices_low.txt']);
+b = load(['surface/', rl, '_triangles_low.txt']);
+c = load(['surface/', rl, '_vertices_high.txt']);
+reg_map = zeros(size(a,1),1);
+not_found = [];
+for i=1:size(a,1)
+    i;
+    [g,e] = min(abs(c(:,1)-a(i,1))+abs(c(:,2)-a(i,2))+abs(c(:,3)-a(i,3)));
+    find_tab = find(corr_right(:,6) == L(e));
+    if isempty(find_tab)
+        not_found = [i, not_found];
+    else
+        reg_map(i) = corr_right(find_tab,5);
+    end
+end
+not_found
+save(['surface/', rl, '_region_mapping_low_not_corrected.txt'],'reg_map', '-ascii' );
