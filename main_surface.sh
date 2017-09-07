d4c0161557c274e3f9a63bcc255affde1944a407
timpx <timpx@eml.cc>
2017-09-07 12:05:48 -0400

    add region mapping for subparcellations

--- main_surface.sh
+++ main_surface.sh
@@ -831,7 +831,7 @@ fi
 
 ## now compute connectivity and length matrix
 if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$ASEG".mif ]; then
-  echo " compute FS labels"
+  echo "compute FS labels"
   labelconvert $PRD/connectivity/aparcaseg_2_diff.nii.gz \
                $FREESURFER_HOME/FreeSurferColorLUT.txt \
                fs_region.txt $PRD/connectivity/aparcaseg_2_diff_fs.mif \
@@ -975,19 +975,23 @@ popd > /dev/null
 if [ -n "$K_LIST" ]; then
   for K in $K_LIST; do
     export curr_K=$(( 2**K ))
-    echo $curr_K
     mkdir -p $PRD/$SUBJ_ID/connectivity_"$curr_K"
     if [ -n "$MATLAB" ]; then
       if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii.gz ]; then
-      $MATLAB -r "run subparcel.m; quit;" -nodesktop -nodisplay 
-      gzip $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii
+        echo "compute subparcellations for $curr_K"
+        $MATLAB -r "run subparcel.m; quit;" -nodesktop -nodisplay 
+        gzip $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii
       fi
     else
-      if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii.gz ]
-      then
-      sh subparcel/distrib/run_subparcel.sh $MCR  
-      gzip $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii
+      if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii.gz ]; then
+        echo "compute subparcellations for subparcellation $curr_K"
+        sh subparcel/distrib/run_subparcel.sh $MCR  
+        gzip $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii
       fi
+    fi
+    if [ ! -f $PRD/$SUBJ_ID/region_mapping_"$curr_K".txt ]; then
+      echo "generate region mapping for subparcellation "$curr_K""
+      python region_mapping_other_parcellations.py
     fi
     if [ ! -f $PRD/connectivity/aparcaseg_2_diff_"$curr_K".mif ]; then
       mrconvert $PRD/connectivity/aparcaseg_2_diff_"$curr_K".nii.gz \
@@ -995,7 +999,7 @@ if [ -n "$K_LIST" ]; then
                    -datatype float32 -force
     fi
     if [ ! -f $PRD/connectivity/weights_"$curr_K".csv ]; then
-      echo "compute connectivity sub matrix using act"
+      echo "compute connectivity matrix using act for subparcellation "$curr_K""
       if [ "$SIFT" = "sift2" ]; then
       # -tck_weights_in flag only needed for sift2 but not for sift/no processing
       tck2connectome $PRD/connectivity/whole_brain_post.tck \
@@ -1013,7 +1017,7 @@ if [ -n "$K_LIST" ]; then
       fi
     fi
     if [ ! -f $PRD/connectivity/tract_lengths_"$curr_K".csv ]; then
-      echo "compute connectivity matrix edge lengths"
+      echo "compute connectivity matrix edge lengths subparcellation "$curr_K""
       view_step=1
       # mean length result: weight by the length, then average
       # see: http://community.mrtrix.org/t/tck2connectome-edge-statistic-sift2-questions/1059/2 
@@ -1027,7 +1031,7 @@ if [ -n "$K_LIST" ]; then
       #fi
     fi
     if [ ! -f $PRD/$SUBJ_ID/connectivity_"$curr_K"/weights.txt ]; then
-      echo "generate files for TVB subparcellations"
+      echo "generate files for TVB subparcellation "$curr_K""
       python compute_connectivity_sub.py $PRD/connectivity/weights_"$curr_K".csv $PRD/connectivity/tract_lengths_"$curr_K".csv $PRD/$SUBJ_ID/connectivity_"$curr_K"/weights.txt $PRD/$SUBJ_ID/connectivity_"$curr_K"/tract_lengths.txt
     fi
     pushd . > /dev/null
